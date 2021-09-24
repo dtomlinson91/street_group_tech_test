94cc22a385319dcfef292374f08ad2814763fdb5
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2021-09-25 00:48:34 +0100

    adding data exploration report + code

--- exploration/report.py
+++ exploration/report.py
@@ -6,7 +6,27 @@ from pandas_profiling import ProfileReport
 
 def main():
     with resources.path("analyse_properties.data", "pp-complete.csv") as csv_file:
-        df_report = pd.read_csv(csv_file)
+        df_report = pd.read_csv(
+            csv_file,
+            names=[
+                "transaction_id",
+                "price",
+                "date_of_transfer",
+                "postcode",
+                "property_type",
+                "old_new",
+                "duration",
+                "paon",
+                "saon",
+                "street",
+                "locality",
+                "town_city",
+                "district",
+                "county",
+                "ppd_category",
+                "record_status",
+            ],
+        )
     profile = ProfileReport(df_report, title="Price Paid Data", minimal=True)
     profile.to_file("price_paid_data_report.html")
 
