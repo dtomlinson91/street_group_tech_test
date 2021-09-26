2a43ea19463190f79c82486807e24a942a42c617
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2021-09-26 01:18:23 +0100

    adding download script for data set

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+# Full data set
+wget https://storage.googleapis.com/street-group-technical-test-dmot/pp-complete.csv -P analyse_properties/data/input
+
+# Monthly update data set
+# wget https://storage.googleapis.com/street-group-technical-test-dmot/pp-monthly-update-new-version.csv -P analyse_properties/data/input
