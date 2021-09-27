7db1edb90c3c447218d5d25479064dbb006935d2
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2021-09-27 03:18:33 +0100

    updating download_data script with pp-2020 dataset

--- download_data.sh
+++ download_data.sh
@@ -2,4 +2,7 @@
 # wget https://storage.googleapis.com/street-group-technical-test-dmot-euw1/input/pp-complete.csv -P data/input
 
 # Monthly update data set
-wget https://storage.googleapis.com/street-group-technical-test-dmot-euw1/input/pp-monthly-update-new-version.csv -P data/input
+# wget https://storage.googleapis.com/street-group-technical-test-dmot-euw1/input/pp-monthly-update-new-version.csv -P data/input
+
+# 2020 data set
+wget https://storage.googleapis.com/street-group-technical-test-dmot-euw1/input/pp-2020.csv -P data/input
